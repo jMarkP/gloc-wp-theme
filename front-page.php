0e6f6697023b9461186dc2cfa3777a98fb6ea7fb
Mark Pim <j.mark.pim@gmail.com>
2022-01-31 18:20:30 +0000

    Start of menu bar work

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The front page of the whole site
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package gloc_2022
+ */
+
+get_header( 'front-page' );
+?>
