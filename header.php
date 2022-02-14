88e4b11065e5db49ff9162877957d9c25ac508f0
Mark Pim <j.mark.pim@gmail.com>
2022-02-14 23:49:14 +0000

    WIP

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,42 +13,44 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,wght@0,600;1,200&display=swap"
+    rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;400;700&display=swap" rel="stylesheet">
+
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'gloc-2022' ); ?></a>
+  <?php wp_body_open(); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$gloc_2022_description = get_bloginfo( 'description', 'display' );
-			if ( $gloc_2022_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $gloc_2022_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+  <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'gloc-2022'); ?></a>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gloc-2022' ); ?></button>
-			<?php
+  <header class="header-bar">
+
+    <div class="site-logo">
+      <?php 
+    the_custom_logo(); 
+    ?>
+    </div>
+    <div class="header-rows">
+      <div class="site-branding header-row">
+        <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+        <h3 class="site-description"><?php bloginfo( 'description' );?></h3>
+      </div>
+
+      <div class="header-row nav-row">
+        <nav id="site-navigation" class="main-navigation">
+          <button class="menu-toggle" aria-controls="primary-menu"
+            aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gloc-2022' ); ?></button>
+          <?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
@@ -55,5 +58,10 @@
 				)
 			);
 			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        </nav><!-- #site-navigation -->
+      </div>
+    </div>
+  </header>
+
+
+  <div id="page" class="site">
