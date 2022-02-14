a35bd8d5cd84629d1a44548f46e0aa771b086c43
Mark Pim <j.mark.pim@gmail.com>
2022-02-14 22:06:24 +0000

    Hero image

--- header-front-page.php
+++ header-front-page.php
@@ -33,26 +33,24 @@
 
   <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'gloc-2022'); ?></a>
 
-  <header class="header-bar">
-    <div class="header-row">
+  <header class="header-bar hero-image">
 
-      <div class="site-logo">
-        <?php 
-			the_custom_logo(); 
-			?>
-      </div>
-
-      <div class="site-title">
+    <div class="site-logo">
+      <?php 
+    the_custom_logo(); 
+    ?>
+    </div>
+    <div class="header-rows">
+      <div class="site-title header-row">
         <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
         <h3 class="site-description"><?php bloginfo( 'description' );?></h3>
       </div>
-    </div>
 
-    <div class="header-row">
-      <nav id="site-navigation" class="main-navigation">
-        <button class="menu-toggle" aria-controls="primary-menu"
-          aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gloc-2022' ); ?></button>
-        <?php
+      <div class="header-row">
+        <nav id="site-navigation" class="main-navigation">
+          <button class="menu-toggle" aria-controls="primary-menu"
+            aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gloc-2022' ); ?></button>
+          <?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
@@ -60,10 +58,10 @@
 				)
 			);
 			?>
-      </nav><!-- #site-navigation -->
+        </nav><!-- #site-navigation -->
+      </div>
     </div>
   </header>
 
 
   <div id="page" class="site">
-		
